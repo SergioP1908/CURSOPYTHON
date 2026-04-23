5b451c74755b1f8d32d4e6e28253bf482aa8dcc2
SergioP1908 <sergiop1908@gmail.com>
2026-04-23 19:58:02 +0200

    ejercicios

--- Codigo Python/ejercicio_while.py
+++ Codigo Python/ejercicio_while.py
@@ -18,14 +18,26 @@ contrasenaInicial=contrasena()
 
 primerUsuario = usuarioInicial
 primerContrasena = contrasenaInicial
-print(usuarioInicial + contrasenaInicial)
+minisculas = [char for char in contrasenaInicial if char.islower()]
+cuantasmin = len(minisculas)
 
-while (len(contrasenaInicial) <8):
-    tamano= len (contrasenaInicial)
-    print(str(tamano))
+while (len(contrasenaInicial) <8 or cuantasmin<1):
+    print("Contraseña Incorrecta, vuelve a introducir la contraseña")
+    #tamano= len (contrasenaInicial)
+    minisculas = [char for char in contrasenaInicial if char.islower()]
+    cuantasmin = len(minisculas)
+    print(str(cuantasmin))
     contrasenaInicial=contrasena()
-    print(contrasenaInicial)
 
+ 
+if (contrasenaInicial.isupper):
+    print("Contraseña correcta")
+else:
+    print("Vuelve aintroducir la contraseña")
 print("fin de bucle")
 
 print("Su primer usuario era : "+ primerUsuario +"\nSu primera contraseña era : " +primerContrasena)
+
+minisculas = [char for char in contrasenaInicial if char.islower()]
+cuantasmin = len(minisculas)
+print(cuantasmin)
